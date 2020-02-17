21bb70467a53cef923a148d5ce8020e71963eb17
Anastasia <nastyakarbovskaya1@gmail.com>
2020-02-17 20:11:06 +0300

    feat: update with dream team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let teamName = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] == 'string') {
+      teamName += members[i].split(' ').join('')[0];
+    }
+  }
+  return teamName.toUpperCase().split('').sort().join('');
 };
